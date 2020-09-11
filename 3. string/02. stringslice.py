03486d760c63587f2dbf081a38b7c7ff9af0e7b1
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-09-11 23:31:33 +0900

    retrieved from @eona1301 on github

---
+++ 3. string/02. stringslice.py
@@ -0,0 +1,10 @@
+license_plate="24가 2210"
+
+print(F"마지막 4글자 :  {license_plate[-4:]} 입니다. ")
+
+"""
+last n => -n
+without : (i.e. [-4] >> output : 마지막 4글자 :  2 입니다.
+
+original output : 마지막 4글자 :  2210 입니다. 
+"""
