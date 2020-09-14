728a73a6932a97727b50dd230981e2ef305823db
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-09-14 15:16:41 +0900

    retrieved from @eona1301 on github

---
+++ 3. string/05. replace.py
@@ -0,0 +1,4 @@
+phonenumber = "010-1111-2222"
+phonenumber = phonenumber.replace("-","")
+
+print(f"phone number {phonenumber}")
---
+++ 3. string/07. split.py
@@ -0,0 +1,15 @@
+url = "http://sharebook.kr"
+urllist = url.split(".")
+
+print(f"url = {urllist[-1]}.")
+
+"""
+-1
+output: url = kr.
+
+0
+output: url = http://sharebook.
+
+
+.을 기준으로 해서 나누고, -1이니까
+"""
