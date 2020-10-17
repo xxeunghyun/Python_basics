85ca306078f4a70877a92af56bb97eda2b43b233
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-10-17 23:21:39 +0900

    retrieved from @eona1301 on github

---
+++ 7. control/13. int(input).py
@@ -0,0 +1,12 @@
+num = int(input("Enter input:"))
+
+print(num)
+
+if (num%2 == 0):
+    print("Even number")
+
+else:
+    print("Odd number")
+"""
+if( ~~ )  or if ~~ all okay
+"""
---
+++ 7. control/14. int(input).py
@@ -0,0 +1,22 @@
+num = int(input(">> 입력값: "))
+
+num += 20
+
+if num>255:
+    num = 255
+
+print(F"출력값: {num}")
+
+
+
+"""
+>>> 
+ RESTART: /Users/seunghyunmin/Desktop/sh/코딩/python/@eona1301/7. control/14. int(input).py 
+>> 입력값: 70
+출력값: 90
+>>> 
+ RESTART: /Users/seunghyunmin/Desktop/sh/코딩/python/@eona1301/7. control/14. int(input).py 
+>> 입력값: 240
+출력값: 255
+>>>
+"""
