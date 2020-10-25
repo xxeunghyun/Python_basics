cca5450f001ba274ea954f40dcb6b411af52c0ff
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-10-25 19:06:26 +0900

    retrieved from @eona1301 on github

---
+++ 7. control/21. upperloweralphabet.py
@@ -0,0 +1,22 @@
+data = input("Enter:\n")
+
+if data.islower() == True:
+    print(data.upper())
+
+else:
+    print(data.lower())
+
+"""
+Enter:
+j
+J
+>>> 
+ RESTART: /Users/seunghyunmin/Desktop/sh/코딩/python/@eona1301/7. control/21. upperlower.py 
+Enter:
+J
+j
+
+
+
+convert lower to upper and upper to lower alphabet inputs
+"""
