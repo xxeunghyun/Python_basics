d2c989f9f9f1e13d51f2c382fefd0fc69ef5a6de
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-11-02 23:32:21 +0900

    얘도 먼말인지 반만 알겠음

---
+++ 7. control/28. split[][;].py
@@ -0,0 +1,18 @@
+num = input("ID?: ")
+
+if 0<= int(num.split('-') [1] [1:3]) <=8:
+    print("Born in Seoul")
+
+else:
+    print("Not born in Seoul")
+
+
+"""
+ID?: 980707-202020
+Born in Seoul
+>>> 
+ RESTART: /Users/seunghyunmin/Desktop/sh/코딩/python/@eona1301/7. control/28. split[][;].py 
+ID?: 960909-292020
+Not born in Seoul
+>>>
+"""
