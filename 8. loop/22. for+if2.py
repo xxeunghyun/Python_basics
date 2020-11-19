eb636a66fb5c20b242323c7e4740524ec37a8d39
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-11-19 21:44:29 +0900

    retrieved from @eona1301 on github

---
+++ 8. loop/22. for+if2.py
@@ -0,0 +1,9 @@
+data = [3, 100, 23, 44]
+
+for i in data:
+    if i%3 == 0:
+        print(i)
+
+'''
+3
+'''
---
+++ 8. loop/24. for+if(len).py
@@ -0,0 +1,15 @@
+data = ["I", "study", "python", "language", "!"]
+
+for i in data:
+    if len(i) >= 3:
+        print(i)
+
+
+'''
+exp: s, p, l
+
+real:
+study
+python
+language
+'''
