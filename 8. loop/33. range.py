d1ca07f024ca5ff6fbead480c0e0121ea43edd96
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2020-11-28 20:57:38 +0900

    retrieved from @eona1301 on github

---
+++ 8. loop/33. range.py
@@ -0,0 +1,15 @@
+pricelist = [32130, 32150, 32000, 32500]
+
+for i in range(len(pricelist)-1,-1,-1):
+    print(i,pricelist[i])
+
+
+'''
+3 32500
+2 32000
+1 32150
+0 32130
+
+descending order regardless its size
+from the end of the range to the first number
+'''
