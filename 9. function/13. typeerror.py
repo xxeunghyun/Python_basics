3a777089aa3219e6cb8dc4d38160489607ce41ba
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-01-03 13:07:07 +0900

    Add files via upload

---
+++ 9. function/13. typeerror.py
@@ -0,0 +1,13 @@
+def func(abc):
+    print(abc)
+
+func()  #need some argument in the bracket
+
+
+'''
+error.py 
+Traceback (most recent call last):
+  File "/Users/seunghyunmin/Desktop/sh/코딩/python/@eona1301/9. function/13. typeerror.py", line 4, in <module>
+    func()
+TypeError: func() missing 1 required positional argument: 'abc'
+'''
