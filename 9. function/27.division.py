2cfb931d9b5d172fb9e9ab6b3252033f86517a76
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-01-09 18:35:51 +0800

    Add files via upload

---
+++ 9. function/27.division.py
@@ -0,0 +1,22 @@
+def calcsalary(annualsalary):
+    print(annualsalary//12)
+
+
+print("Monthly Salary given = ")
+calcsalary(46000000)
+
+
+'''
+when annualsalary = 1200000
+if salary//12 = 1000000
+if salary/12 = 1000000.0
+'''
+
+'''
+//12 = 3833333
+/12 = 3833333.3333333335
+'''
+
+'''
+소수점 차이
+'''
---
+++ 9. function/29.bfirst.py
@@ -0,0 +1,11 @@
+def direction(a,b):
+    print("LEFT: ", a)
+    print("RIGHT: ",b)
+
+direction(b=120, a=200)
+
+
+'''
+LEFT:  200
+RIGHT:  120
+'''
