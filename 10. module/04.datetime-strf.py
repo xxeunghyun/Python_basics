67b1263f634083c01d63d565d195072166c427c6
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-01-15 22:30:03 +0800

    Add files via upload

---
+++ 10. module/04.datetime-strf.py
@@ -0,0 +1,14 @@
+import datetime
+
+now = datetime.datetime.now()
+print(now.strftime("%H:%M:%S"))
+
+'''
+>>> 
+ RESTART: /Users/seunghyunmin/Desktop/sh/코딩/python/@eona1301/10. module/04.datetime-strf.py 
+22:28:59
+>>> 
+ RESTART: /Users/seunghyunmin/Desktop/sh/코딩/python/@eona1301/10. module/04.datetime-strf.py 
+22:29:06
+>>>
+'''
