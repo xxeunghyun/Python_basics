a12ab2f52826d32ab885ab75798837c9d8ee38dd
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-01-25 20:32:35 +0800

    Add files via upload

---
+++ 11. class/03.classinit.py
@@ -0,0 +1,5 @@
+class Human:
+    def __init__(self):
+        print("HEEHHHE")
+
+areum = Human()
---
+++ 11. class/04.initdata.py
@@ -0,0 +1,12 @@
+class Human:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        print(name,age,gender)
+
+areum = Human("areum",25,"F")
+
+'''
+areum 25 F
+'''
