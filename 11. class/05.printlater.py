7bc828cfdc8d13c20d6e2ec42c10c476dd63b8f6
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-01-26 22:54:27 +0800

    Add files via upload

---
+++ 11. class/05.printlater.py
@@ -0,0 +1,9 @@
+class Human:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+
+areum = Human("Areum", 25, "F")
+print(areum.age)
