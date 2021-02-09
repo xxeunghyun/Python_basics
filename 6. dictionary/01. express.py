47310e6be86a5fac11f4efc6d5da11bd1f084d70
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-02-09 21:00:40 +0800

    Delete 6. dictionary directory

--- 6. dictionary/01. express.py
+++
@@ -1,9 +0,0 @@
-*data, _, _ = (8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5, 7.8, 9.4)
-
-print(data)
-
-
-"""
-automatically sorted in ascending order
-[8.8, 8.9, 8.7, 9.2, 9.3, 9.7, 9.9, 9.5]
-"""
--- 6. dictionary/19. dict(zip).py
+++
@@ -1,7 +0,0 @@
-keys = ("apple", "pear", "peach")
-vals = (300, 250, 400)
-print(dict(zip(keys, vals)))
-
-"""
-{'apple': 300, 'pear': 250, 'peach': 400}
-"""
