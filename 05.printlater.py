3b55e6907c1f0723c7ccfe7855e82a19849d739a
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-04-13 21:48:15 +0800

    Delete 05.printlater.py

--- 05.printlater.py
+++
@@ -1,9 +0,0 @@
-class Human:
-    def __init__(self, name, age, gender):
-        self.name = name
-        self.age = age
-        self.gender = gender
-
-
-areum = Human("Areum", 25, "F")
-print(areum.age)
