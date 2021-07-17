a3d2ff070d6717cd17d7c1e4066f81ae2b9f3ede
seunghyunmin <68652374+xxeunghyun@users.noreply.github.com>
2021-07-17 21:12:51 +0900

    Add files via upload

---
+++ 11. class/16.withdrawl.py
@@ -0,0 +1,51 @@
+class Account:
+    account_cnt = 0
+
+    def __init__(self, name, balance):
+        self.bankname = "HSBC"
+        self.name = name
+        self.balance = balance
+        self.account_num = self.set_account() #이전 예제랑 다른 점 여기!!
+
+        Account.account_cnt += 1
+
+    def __str__(self):
+        return F"총 등록 인원 : {Account.account_cnt}\n은행이름 : {self.bankname}\n예금주 : {self.name}\n계좌번호 : {self.account_num}\n잔액 : {self.balance}"
+
+    def set_account(self):
+        import random
+        tmp = ""
+
+        for num in [3,2,6]:
+            for _ in range(num):
+                tmp += str(random.randint(0,9))
+            tmp += "-"
+
+        return tmp[:len(tmp)-1]
+
+    def get_account_num(self):
+        print(Account.account_cnt)
+
+    def deposit(self):
+        while True:
+            coin = int(input("값을 입력하세요: "))
+
+            if coin>=1 :
+                self.balance += coin
+            else:
+                print("최소 1원 이상을 입력해야 합니다.")
+
+    def withdraw(self):
+        while True:
+            coin = int(input("출금하고자 하는 값을 입력하세요: "))
+
+            if coin > self.balance:
+                print(F"현재 잔고는 {self.balance}입니다.\n잔고 이상으로 출금할 수 없습니다.")
+
+            else:
+                self.balance -= coin
+                break
+            
+
+
+account = Account("홍길동", 3000)
